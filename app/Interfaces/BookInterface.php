1c7ef9ecf8bf16ceaf00905b77661a11664437c6
Algorithm <samuel@nopeus.solutions>
2021-06-27 13:22:43 +0100

    Fixed Undefined allBooks method bug on BookInterface

--- app/Interfaces/BookInterface.php
+++ app/Interfaces/BookInterface.php
@@ -7,6 +7,7 @@ namespace App\Interfaces;
 interface BookInterface
 {
     public function getBooks();
+    public function allBooks();
     public function getBook($book_id);
     public function storeBook($bookData);
     public function findByID($book_id);
